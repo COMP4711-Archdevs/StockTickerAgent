28dce2f8e773e1c2e7963e7188890a4619df6e42
hahoanghai <hahoanghai.ueh@gmail.com>
2016-04-16 11:46:40 -0700

    get all stock from server

--- application/models/Stock.php
+++ application/models/Stock.php
@@ -10,4 +10,48 @@ class Stock extends MY_Model {
     function __construct() {
         parent::__construct('stocks','code');
     }
+
+     function getAllStocksFromServer(){
+       $allStockData = $this->readFromURL("http://bsx.jlparry.com/data/stocks");
+        
+       $allStock = array();
+
+       for($i = 1; $i < count($allStockData); $i++){
+            $stockInfo = array();
+            
+            $stockInfo["Code"] = $allStockData[$i][0];
+            $stockInfo["Name"] = $allStockData[$i][1];
+            $stockInfo["Value"] = $allStockData[$i][3];
+            
+            array_push($allStock,$stockInfo);
+        } 
+        
+        return $allStock; 
+    }
+
+
+
+
+
+
+     public function readFromURL($url)
+    {
+        $content = array();
+       // $row = 1;
+
+        if (($handle = fopen($url, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $num = count($data);
+               
+            //    $row++;
+                $temp = array();
+                for ($i=0; $i < $num; $i++) {
+                    $temp[] = $data[$i];
+                }
+                $content[] = $temp;
+            }
+            fclose($handle);
+        }
+        return $content;
+    }
 }

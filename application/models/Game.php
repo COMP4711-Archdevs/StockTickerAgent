cfbbf9b67fe99e5fe44868fa972558ad30c76095
hahoanghai <hahoanghai.ueh@gmail.com>
2016-04-17 12:41:32 -0700

    get stock quantity belong to one player from database

--- application/models/Game.php
+++ application/models/Game.php
@@ -7,5 +7,11 @@ class Game extends MY_Model {
         parent::__construct('holdingstock','username');
     }
 
-    
+    public function getStockQuantityBelongToPlayer($player,$code){
+    	$sql = "SELECT quantity FROM holdingstock WHERE username = '{$player}' AND stockcode = '{$code}' LIMIT 1";
+	$result = $this->db->query($sql);
+	$row = $result->row();
+
+	return ($result->num_rows() === 1 && $row->quantity) ? $row->quantity : false;
+    }
 }
